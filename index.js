6e9609228c46b10142abfe1b0e7f83d3ecfac24b
emonmahdi <developeremon1997@gmail.com>
2024-03-28 11:30:12 +0600

    all api done

---
+++ index.js
@@ -0,0 +1,125 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const uri = process.env.DATABASE_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = await client.db("taskmaster");
+    const tasksCollection = db.collection("tasks");
+    const usersCollection = db.collection("users");
+
+    console.log("Successfully connected to MongoDB!");
+
+    app.get("/", (req, res) => {
+      res.send("Task Master Server");
+    });
+
+    app.get("/tasks", async (req, res) => {
+      try {
+        const tasks = await tasksCollection.find({}).toArray();
+        res.json(tasks);
+      } catch (err) {
+        console.error("Error fetching tasks:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    // find tasks filtering by email
+    app.get("/tasks/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+        // Find tasks associated with the given email
+        const tasks = await tasksCollection
+          .find({ assignedTo: email })
+          .toArray();
+        res.json(tasks);
+      } catch (error) {
+        console.error("Error fetching tasks by email:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.post("/tasks", async (req, res) => {
+      const newTask = req.body;
+
+      try {
+        const result = await tasksCollection.insertOne(newTask);
+        res.status(201).json(result);
+      } catch (err) {
+        console.error("Error creating task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    // user post api
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+
+      try {
+        const result = await usersCollection.insertOne(newUser);
+        res.status(201).json(result);
+      } catch (err) {
+        console.error("Error creating task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.get("/users", async (req, res) => {
+      try {
+        const users = await usersCollection.find({}).toArray();
+        res.send(users);
+      } catch (error) {
+        res.status(500).json({ error: "Internal Server Error!" });
+      }
+    });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params?.id;
+      const result = await tasksCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    app.patch("/tasks/:id", async (req, res) => {
+      const taskId = req.params.id;
+      const updatedTaskData = req.body;
+
+      try {
+        const result = await tasksCollection.updateOne(
+          { _id: new ObjectId(taskId) },
+          { $set: updatedTaskData }
+        );
+
+        if (result.matchedCount === 0) {
+          res.status(404).json({ error: "Task not found" });
+        } else {
+          res.json({ message: "Task updated successfully" });
+        }
+      } catch (err) {
+        console.error("Error updating task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
