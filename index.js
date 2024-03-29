63a3ba8f7a3e4fdf3243867f5f9ed38016856f89
emonmahdi <developeremon1997@gmail.com>
2024-03-29 19:02:06 +0600

    cors url add

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    url: [
+      "http://localhost:3000",
+      "https://task-management-server-6jswwgpbm.vercel.app",
+    ],
+  })
+);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e5zetpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
